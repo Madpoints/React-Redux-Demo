d5ff45bf843d6688a20446487942c797b0f6522a
John Furtado <jf8846@gmail.com>
2018-08-21 18:54:35 -0400

    tutorial 12: nesting components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Witchers from './Witchers'
 
 class App extends Component {
 	render() {
@@ -6,6 +7,7 @@ class App extends Component {
 			<div className="App">
 				<h1>My first React app</h1>
 				<p>Hello World</p>
+				<Witchers/>
 			</div>
 		);
 	}
---
+++ my-app/src/Witchers.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Witchers extends Component {
+	render() {
+		return(
+			<div className="witcher">
+				<div>Name: Geralt</div>
+				<div>Age: 90+</div>
+				<div>School: Wolf</div>		
+			</div>		
+		)
+	}
+}
+
+export default Witchers
