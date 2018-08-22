a03bc39c47627011feacd7aba068acea0dd56ab5
John Furtado <jf8846@gmail.com>
2018-08-21 20:21:38 -0400

    ui(function) and container(class) components

--- my-app/src/Witchers.js
+++ my-app/src/Witchers.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Witchers extends Component {
-	render() {
-		const {witchers} = this.props
-		const witcherList = witchers.map(witcher => {
-			return(
-				<div className="witcher" key={witcher.id}>
-					<div>Name: {witcher.name}</div>
-					<div>Age: {witcher.age}</div>
-					<div>School: {witcher.school}</div>	
-					<hr></hr>	
-				</div>	
-			)	
-		})
+const Witchers = ({witchers}) => {
+	const witcherList = witchers.map(witcher => {
 		return(
-			<div className="witcher-list">
-				{witcherList}
+			<div className="witcher" key={witcher.id}>
+				<div>Name: {witcher.name}</div>
+				<div>Age: {witcher.age}</div>
+				<div>School: {witcher.school}</div>	
+				<hr></hr>	
 			</div>	
-		)
-	}
+		)	
+	})
+	return(
+		<div className="witcher-list">
+			{witcherList}
+		</div>	
+	)
 }
 
 export default Witchers
