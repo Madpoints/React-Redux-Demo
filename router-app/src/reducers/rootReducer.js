8d948c867aa5fc0524ec13f46af5b576f47511f2
John Furtado <jf8846@gmail.com>
2018-09-11 18:54:16 -0400

    mapping state to props

--- router-app/src/reducers/rootReducer.js
+++ router-app/src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-  posts: []
+  posts: [
+    {id: '1', name: 'Geralt of Rivia', school: 'Wolf'},
+    {id: '2', name: 'Letho of Gullet', school: 'Viper'},
+    {id: '3', name: 'Gerd', school: 'Bear'}
+  ]
 }
 
 const rootReducer = (state = initState, action) => {
